67483c06c271bacbd6777dcd05c067c905985d69
1stAdityaSri <1staditya.synthdatasolutions@gmail.com>
2025-07-26 12:56:54 +0530

    cvs

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ app.use(express.json()); // <-- add this line before your routes
 app.use(express.urlencoded({ extended: true }));
 const path = require('path');
 
-// Serve static files from /images/uploads
-app.use('/images/uploads', express.static(path.join(__dirname, 'images/uploads')));
-
-console.log('CORS Origin:', process.env.ORIGINKA);
 app.use(cors({
   origin: process.env.ORIGINKA,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true // only if you’re using cookies or sessions
 }));
+// Serve static files from /images/uploads
+app.use('/images/uploads', express.static(path.join(__dirname, 'images/uploads')));
+
+console.log('CORS Origin:', process.env.ORIGINKA);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
