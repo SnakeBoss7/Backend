8cacaf7336aff30043cc88e05d8feb0a7685004e
1stAdityaSri <1staditya.synthdatasolutions@gmail.com>
2025-07-26 20:42:43 +0530

    uri

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb+srv://1stadityaa:HtPNjemh9adSJMAq@newwala.aembtih.mongodb.net/Social`, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
     console.log(`MongoDB Connected:${conn.connection.host}`);
