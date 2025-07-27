576df852ed59d8ad3d7289bc41f65d1f2e088ac7
SnakeBoss7 <rahuldharwal12005@gmail.com>
2025-07-27 15:36:25 +0530

    coomit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/images/uploads', express.static(path.join(__dirname, 'images/uploads')
 
 
 app.use(cors({
-  origin: 'http://localhost:3000',  // React frontend origin
+  origin: 'https://new-pdu95vuau-insaens-projects.vercel.app',  // React frontend origin
   credentials: true                 // allow sending cookies
 }));
 
